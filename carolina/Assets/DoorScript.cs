8de13eace362ce9c5e1ab23d734378b5caac4abe
Chris Caragianis <chriscaragianis@gmail.com>
2019-01-15 02:50:09 -0500

    door opens

---
+++ carolina/Assets/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	private Animator _animator;
+
+	void Start() {
+		_animator = GetComponent<Animator> ();
+		_animator.SetBool ("open", false);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log(_animator.GetBool("open"));	
+		Debug.Log ("collided");
+		_animator.SetBool ("open", true);
+		Debug.Log(_animator.GetBool("open"));
+	}
+}
